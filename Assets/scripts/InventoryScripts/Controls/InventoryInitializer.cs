473ec6c79c590b2f90049fd0465fb6ffd17cb7c4
Bradley Dowling <bradley.dowling@wsu.edu>
2022-02-27 10:26:26 -0800

    Update InventoryInitializer.cs
    Added functions to create necessary components in each of the game objects

--- Assets/scripts/InventoryScripts/Controls/InventoryInitializer.cs
+++ Assets/scripts/InventoryScripts/Controls/InventoryInitializer.cs
@@ -17,13 +17,23 @@ using UnityEngine;
 
 public class InventoryInitializer : MonoBehaviour
 {
+    #region attributes
     private GameObject parentCanvas;
     private GameObject inventoryPanel;
     private GameObject slotPanel;
     private GameObject displayPanel;
     private GameObject controlPanel;
     private GameObject inventoryObject;
+    private GameObject displayImage;
+    private GameObject displayName;
+    private GameObject displayDescription;
+    private GameObject equipButton;
+    private GameObject dropButton;
+    private GameObject putAwayButton;
+    private GameObject inspectButton;
+
     private Inventory inventory;
+    #endregion
 
     // Start is the function used to initialize all UI elements, as well as the 
     // actual inventory...
@@ -42,54 +52,117 @@ public class InventoryInitializer : MonoBehaviour
         // Create Inventory Panel if it doesn't exist
         if (!parentCanvas.transform.Find("InventoryPanel"))
         {
-            inventoryPanel = CreatePanel("InventoryPanel", parentCanvas);
+            inventoryPanel = CreatePanel("InventoryPanel");
+            inventoryPanel.transform.SetParent(parentCanvas.transform);
             SetBoundaries(inventoryPanel.GetComponent<RectTransform>(), 10f, 10f, 10f, 10f);
         }
 
         // Create the slot panel if it doesn't exist
         if (!inventoryPanel.transform.Find("SlotPanel"))
         {
-            slotPanel = CreatePanel("SlotPanel", inventoryPanel);
+            slotPanel = CreatePanel("SlotPanel");
+            slotPanel.transform.SetParent(inventoryPanel.transform);
             SetBoundaries(slotPanel.GetComponent<RectTransform>(), 20f, 500f, 20f, 20f);
             slotPanel.GetComponent<UnityEngine.UI.Image>().color = new Color(255f, 0f, 0f);
             slotPanel.AddComponent<InventorySlotPanel>();
         }
 
         // Create the slots in the slot panel
+        // TODO: Functional reformatting?
         for (int i = 1; i <= inventory.GetMaxItemCount(); i++)
         {
-            GameObject slot = new GameObject("Item" + i);
-            slot.AddComponent<SlotPanel>();
+            GameObject slot = CreateItemSlot("Item" + i, slotPanel);
         }
 
         // Create the display panel if it doesn't exist
         if (!inventoryPanel.transform.Find("DisplayPanel"))
         {
-            displayPanel = CreatePanel("DisplayPanel", inventoryPanel);
+            displayPanel = CreatePanel("DisplayPanel");
+            displayPanel.transform.SetParent(inventoryPanel.transform);
             SetBoundaries(displayPanel.GetComponent<RectTransform>(), 20f, 20f, 100f, 300f);
             displayPanel.GetComponent<UnityEngine.UI.Image>().color = new Color(0f, 255f, 0f);
             displayPanel.AddComponent<InventoryDisplayPanel>();
         }
 
+        // Create display panel elements
+        displayImage = CreatePanel("DisplayImage");
+        displayImage.transform.SetParent(displayPanel.transform);
+
+        displayName = CreateTextObject("DisplayName");
+        displayName.transform.SetParent(displayPanel.transform);
+
+        displayDescription = CreateTextObject("DisplayDescription");
+        displayDescription.transform.SetParent(displayPanel.transform);
+
         // Create the control panel if it doesn't exist
         if (!inventoryPanel.transform.Find("ControlPanel"))
         {
-            controlPanel = CreatePanel("ControlPanel", inventoryPanel);
+            controlPanel = CreatePanel("ControlPanel");
+            controlPanel.transform.SetParent(inventoryPanel.transform);
             SetBoundaries(controlPanel.GetComponent<RectTransform>(), 500f, 20f, 20f, 300f);
             controlPanel.GetComponent<UnityEngine.UI.Image>().color = new Color(0f, 0f, 255f);
             controlPanel.AddComponent<InventoryControlPanel>();
         }
+
+        // Create the control panel buttons
+        equipButton = CreateButton("EquipButton", controlPanel);
+        putAwayButton = CreateButton("PutAwayButton", controlPanel);
+        dropButton = CreateButton("DropButton", controlPanel);
+        inspectButton = CreateButton("InspectButton", controlPanel);
     }
 
-    public GameObject CreatePanel(string name, GameObject parent)
+    public GameObject CreatePanel(string name)
     {
         GameObject newGameObj = new GameObject(name);
-        newGameObj.transform.SetParent(parent.transform);
         newGameObj.AddComponent<RectTransform>();
         newGameObj.AddComponent<CanvasRenderer>();
         newGameObj.AddComponent<UnityEngine.UI.Image>();
 
         return newGameObj;
+    }
+
+    public GameObject CreateTextObject(string name)
+    {
+        GameObject textObj = new GameObject(name);
+        textObj.AddComponent<RectTransform>();
+        textObj.AddComponent<CanvasRenderer>();
+        textObj.AddComponent<UnityEngine.UI.Text>();
+
+        return textObj;
+    }
+
+    public GameObject CreateButton(string name, GameObject parent)
+    {
+        // Create button GameObject
+        GameObject button = CreatePanel(name);
+        button.AddComponent<UnityEngine.UI.Button>();
+
+        // Create text GameObject for button
+        GameObject buttonText = CreateTextObject("Text");
+
+        buttonText.transform.SetParent(button.transform);
+        button.transform.SetParent(parent.transform);
+
+        return button;
+    }
+
+    public GameObject CreateItemSlot(string name, GameObject parent)
+    {
+        GameObject slot = CreatePanel(name);
+        slot.transform.SetParent(parent.transform);
+
+        // Create individual slot elements
+        GameObject slotName = CreateTextObject("SlotName");
+        slotName.transform.SetParent(slot.transform);
+
+        GameObject slotDescription = CreateTextObject("SlotDescription");
+        slotDescription.transform.SetParent(slot.transform);
+
+        GameObject slotIcon = CreatePanel("SlotIcon");
+        slotIcon.transform.SetParent(slot.transform);
+
+        slot.AddComponent<SlotPanel>();
+        return slot;
     }
 
     public void SetBoundaries(RectTransform panelRT, float top, float right, float bottom, float left)

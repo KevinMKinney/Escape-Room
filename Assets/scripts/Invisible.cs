e58ff2f40d4c0965813e0f24400b43157ddc3451
Bradley Dowling <bradley.dowling@wsu.edu>
2022-04-15 23:04:59 -0700

    Added comment describing constructor purpose

--- Assets/scripts/Invisible.cs
+++ Assets/scripts/Invisible.cs
@@ -6,6 +6,9 @@ public class Invisible : MonoBehaviour
 {
     public bool isInvisible;
 
+
+    // Brad added the following constructor to avoid the "null object reference" error if
+    // isInvisible was not set within the inspector window
     public void Start()
     {
         isInvisible = true;

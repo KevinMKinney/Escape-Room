078ef0a6a52d349027fd41805d40663316603a6a
Kevin Kinney <kevin.kinney@gmail.com>
2022-04-21 22:53:46 -0700

    adding more of my changes

--- Assets/scripts/MeshScripts/MeshMap.cs
+++ Assets/scripts/MeshScripts/MeshMap.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using System.Linq;
+using System;
 
-[RequireComponent(typeof(MeshFilter), (typeof(MeshRenderer)))]
 public static class MeshMap
 {
     // creates vertices for mesh based on pre-made noise
-    public static Vector3[] GenerateVerticies(float[,] noiseMap) {
+    public static Vector3[] generateVerticies(float[,] noiseMap) {
         // initialize variables
         int mapWidth = noiseMap.GetLength(0);
         int mapHeight = noiseMap.GetLength(1);
+
+        if (mapWidth <= 1 || mapHeight <= 1) {
+            throw new Exception();
+            //return null;
+        }
 
         Vector3[] vertices = new Vector3[mapWidth*mapHeight];
 
         // Turn noiseMap pixels into vertices (1 pixel = 1 vertex)
         for(int y = 0; y < mapHeight; y++) {
             for(int x = 0; x < mapWidth; x++) {
-                Vector3 v = new(x, (noiseMap[x, y]), y);
+                Vector3 v = new Vector3(x, (noiseMap[x, y]), y);
                 vertices.SetValue(v, (y*mapWidth+x));
             }
         }
@@ -27,7 +32,13 @@ public static class MeshMap
     }
 
     // creates triangles for mesh
-    public static int[] GenerateTriangles(int mapWidth, int mapHeight) {
+    public static int[] generateTriangles(int mapWidth, int mapHeight) {
+
+        if (mapWidth <= 1 || mapHeight <= 1) {
+            throw new Exception();
+            //return null;
+        }
+
         // initialize variables
         int index = 0;
         int j = 0;
@@ -50,37 +61,65 @@ public static class MeshMap
         return triangles;
     }
 
+    public static Vector2[] generateUVs(Mesh mesh, int mapWidth, int mapHeight) {
+        int size = mapWidth*mapHeight;
+        Vector2[] UVs = new Vector2[size];
+
+        for (int i = 0; i < size; i++) {
+            UVs[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].y);
+        }
+        return UVs;
+    }
+
     // calculates the steepness of triangles based on their normal vectors (the functional way)
-    public static float[] calculateSteepness(Mesh mesh) {
-        Vector3[] norm = new Vector3[mesh.vertices.Length];
+    public static float[] calculateSteepness(Mesh mesh, int mapWidth, int mapHeight) {
+        Vector3[] norm = new Vector3[mapWidth*mapHeight];
         norm = mesh.normals;
         return norm.Select(x => Vector3.Angle(x, Vector3.up)).ToArray();
     }
 
-    // returns the max value in float array (used for steepness)
-    public static float GetMaxSteepness(float[] steepVal) {
-        return steepVal.Aggregate((x, y) => Mathf.Max(x, y));
+    // returns the max value in float array
+    public static float getMaxVertex(Vector3[] vertices) {
+        float[] array = vertices.Select(x => x.y).ToArray();
+        return array.Aggregate((x, y) => Mathf.Max(x, y));
+    }
+
+    // returns the min value in float array
+    public static float getMinVertex(Vector3[] vertices) {
+        float[] array = vertices.Select(x => x.y).ToArray();
+        return array.Aggregate((x, y) => Mathf.Min(x, y));
     }
 
     // creates the colors of the mesh based on its steepness
-    public static Color[] GenerateColors(Mesh mesh, float[] steepVal, float snowThresh) {
+    public static Color[] generateColors(Mesh mesh, int mapWidth, int mapHeight, float[] steepVal, float snowThresh, float waterThresh, Color flatCol, Color steepCol) {
         // initialize variables
-        int size = mesh.vertices.Length;
+        int size = mapWidth*mapHeight;
         Color[] colors = new Color[size];
-        // get some random colors
-        Color cool1 = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        Color cool2 = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        // color init
+        //Color flatCol = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        //Color steepCol = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
 
-        float maxSteep = GetMaxSteepness(steepVal);
+        //float maxSteep = steepVal.Aggregate((x, y) => Mathf.Max(x, y));
+        float maxSteep = Mathf.Max(steepVal);
 
         // assign colors
         for (int i = 0; i < size; i++) {
-            if (mesh.vertices[i].y >= snowThresh) {
-                colors[i] = Color.Lerp(Color.white, cool2, (steepVal[i])/maxSteep);
+            if (mesh.vertices[i].y <= waterThresh) {
+                colors[i] = flatCol;
             } else {
-                colors[i] = Color.Lerp(cool1, cool2, (steepVal[i])/maxSteep);
+                if (mesh.vertices[i].y >= snowThresh) {
+                    colors[i] = Color.Lerp(Color.white, steepCol, (steepVal[i])/maxSteep);
+                } else {
+                    colors[i] = Color.Lerp(flatCol, steepCol, (steepVal[i])/maxSteep);
+                }
             }
+
         }
         return colors;
+    }
+
+    // WIP - function for testing computational time
+    public static void getTime(int a) {
+        Debug.Log("Time at "+a+" is: "+Time.time);
     }
 }

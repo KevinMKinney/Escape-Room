5979cc752524ac48bc36aa1185309bce32d9c45d
Bradley Dowling <bradley.dowling@wsu.edu>
2022-04-24 13:03:49 -0700

    Update Invisible.cs

--- Assets/scripts/Invisible.cs
+++ Assets/scripts/Invisible.cs
@@ -11,6 +11,17 @@ public class Invisible : MonoBehaviour
     // isInvisible was not set within the inspector window
     public void Start()
     {
-        isInvisible = true;
+        isInvisible = false;
+    }
+
+    public void Update()
+    {
+        // The following was added for testing purposes. Player can toggle their visibility
+        // to the AI on and off by pressing "i"
+        if (Input.GetKeyDown("i"))
+        {
+            isInvisible = !isInvisible;
+            Debug.Log("Player Visible: " + !isInvisible);
+        }
     }
 }
